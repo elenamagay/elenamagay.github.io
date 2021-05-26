ba27137c4336b8da8dbdbabcfdf9cb2df0f5cedd
elenamagay <elenamagay1985@gmail.com>
2021-05-26 02:29:51 -0400

    added a new project to portfolio

--- src/projects.js
+++ src/projects.js
@@ -1,52 +1,45 @@
 const projects = [
     {
         "id": 1,
-        "name": "Password Generator",
-        "thumbnail": "PasswordGenerator",
-        "github": "https://github.com/elenamagay/homework3",
-        "deployment": "https://elenamagay.github.io/homework3/"
+        "name": "Donation Connection",
+        "thumbnail": "DonationConnection",
+        "github": "https://github.com/kmh8827/DonationConnection",
+        "deployment": "https://donation-connection.herokuapp.com/"
     },
     {
         "id": 2,
-        "name": "Code Quiz",
-        "thumbnail": "CodeQuiz",
-        "github": "https://github.com/elenamagay/homework4",
-        "deployment": "https://elenamagay.github.io/homework4/"
-    },
-    {
-        "id": 3,
-        "name": "Work Day Scheduler",
-        "thumbnail": "WorkDayScheduler",
-        "github": "https://github.com/elenamagay/homework5",
-        "deployment": "https://elenamagay.github.io/homework5/"
-    },
-    {
-        "id": 4,
-        "name": "Weather Dashboard",
-        "thumbnail": "WeatherDashboard",
-        "github": "https://github.com/elenamagay/homework6",
-        "deployment": "https://elenamagay.github.io/homework6/"
-    },
-    {
-        "id": 5,
         "name": "Trivia Trainer",
         "thumbnail": "triviaTrainer",
         "github": "https://github.com/malenchite/trivia-trainer",
         "deployment": "https://malenchite.github.io/trivia-trainer/"
     },
     {
-        "id": 6,
+        "id": 3,
+        "name": "Weather Dashboard",
+        "thumbnail": "WeatherDashboard",
+        "github": "https://github.com/elenamagay/homework6",
+        "deployment": "https://elenamagay.github.io/homework6/"
+    },    
+    {
+        "id": 4,
         "name": "Eat-Da-Burger",
         "thumbnail": "BurgerAppScreenshot",
         "github": "https://github.com/elenamagay/Burger",
         "deployment": "https://eat-da-yummy-burger.herokuapp.com/"
     },
     {
-        "id": 7,
+        "id": 5,
         "name": "Workout Buddy",
         "thumbnail": "workoutBuddy",
         "github": "https://github.com/cparros/project-2",
         "deployment": "https://workout-buddy-unc.herokuapp.com/"
+    },
+    {
+        "id": 6,
+        "name": "Code Quiz",
+        "thumbnail": "CodeQuiz",
+        "github": "https://github.com/elenamagay/homework4",
+        "deployment": "https://elenamagay.github.io/homework4/"
     }
 ];
 
